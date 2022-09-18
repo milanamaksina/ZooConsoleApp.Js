4ae2f9b4b9818573e95f3ee35753fe5f6aa2e50a
Milana Maksina <m_maksinaa@mail.ru>
2022-09-18 19:31:23 +0300

    feat: add methods for veterinarion

--- src/Entities/Employees/Veterinarian.js
+++ src/Entities/Employees/Veterinarian.js
@@ -5,4 +5,17 @@ module.exports.Veterinarian = class Veterinarian extends Employee {
         super(firstName, lastName);
         this.animalExperience = [];
     }
+    AddAnimalExperience(animal) {
+        this.animalExperience.push(animal.constructor.name)
+    }
+    
+    HasAnimalExperience(animal) {
+        return this.animalExperience.includes(animal.constructor.name)
+    }
+    HealAnimal(animal) {
+        if (animal.isSick == true && this.HasAnimalExperience(animal)) {
+           animal.Heal();
+           console.log(`Animal ${animal.constructor.name} was healed`)
+        }
+    }
 }
