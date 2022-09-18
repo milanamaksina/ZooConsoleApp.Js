39c47250982aa91a7685318d5b33614e9d60b152
Milana Maksina <m_maksinaa@mail.ru>
2022-09-19 00:17:33 +0300

    feat: test files

---
+++ __tests__/EntitiesTests/Animals.test.js
@@ -0,0 +1,7 @@
+const { Animal } = require("../../src/Entities/Animals/Animal");
+
+test("Should not be able to create instance of Animal", () => {
+    expect(() => {
+        let animal = new Animal();
+    }).toThrow("Can't create an instance of Animal abstract class");
+});
