daf9893511e41d17b2601ffdb883f61aa5a5d94e
Milana Maksina <m_maksinaa@mail.ru>
2022-09-18 19:26:38 +0300

    feat: add zookeepers methods

--- src/Entities/Employees/ZooKeeper.js
+++ src/Entities/Employees/ZooKeeper.js
@@ -5,4 +5,18 @@ module.exports.ZooKeeper = class ZooKeeper extends Employee {
         super(firstName, lastName);
         this.animalExperience = [];
     }
+    AddAnimalExperience(animal) {
+        this.animalExperience.push(animal.constructor.name)
+      }
+    
+    HasAnimalExperience(animal) {
+        return this.animalExperience.includes(animal.constructor.name)
+    }
+    FeedAnimal(time, animal) {
+        if (this.HasAnimalExperience(animal)) {
+          console.log(`Animal ${animal.constructor.name} was fed`)
+          return animal.Feed(time, this)
+        }
+        return false
+    }
 }
